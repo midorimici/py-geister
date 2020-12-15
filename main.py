bcd4f5f914e85c4fe6f9060aa222fe9b077d9e5d
midori mici <midorimici.works@gmail.com>
2020-12-15 11:23:47 +0900

    :sparkles:mute by command line arg

--- main.py
+++ main.py
@@ -16,9 +16,18 @@ if __name__ == '__main__':
     forbid_snd = snd('./sounds/forbid.wav')
     move_snd = snd('./sounds/move.wav')
 
+    # コマンドライン引数
     args = sys.argv
-    _flag = FULLSCREEN if len(args) == 2 and args[1] == 'f' else 0
+    # [f] フルスクリーン
+    _flag = FULLSCREEN if len(args) >= 2 and 'f' in args[1:] else 0
     screen = pygame.display.set_mode(DISP_SIZE, _flag)
+    # [m] ミュート
+    if len(args) >= 2 and 'm' in args[1:]:
+        select_snd.set_volume(0)
+        decide_snd.set_volume(0)
+        forbid_snd.set_volume(0)
+        move_snd.set_volume(0)
+    
     pygame.display.set_caption('Geister')
     font = pygame.font.SysFont('hg丸ｺﾞｼｯｸmpro', 16)
 
